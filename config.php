a85afdb48a5eccd5d55ec613b59d6fcf98ee3da2
Kang Cahya <cahyadyazin@yahoo.com>
2021-01-26 05:13:48 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,20 +1,20 @@
 <?php
-	// setting hidden file here, define all filename in lowercase.
+	// setting hidden file or folder here, define all filename or foldername in lowercase.
 	defined('CNF_HIDDEN_FILES')      	OR define('CNF_HIDDEN_FILES', [
-																'.htaccess',
-																'.well-known',
-																'cgi-bin',
-																'error_log',
-																'index.php',
-																'404.php',
-																'.git',
-																'license',
-																'readme.md',
-																'assets',
-																'config.php',
-																'data-public',
-																'helper.php'
-															]);
+										'.htaccess',
+										'.well-known',
+										'cgi-bin',
+										'error_log',
+										'index.php',
+										'404.php',
+										'.git',
+										'license',
+										'readme.md',
+										'assets',
+										'config.php',
+										'data-public',
+										'helper.php'
+									]);
 
 	// setting directory location to scan
 	defined('CNF_DEFAULT_SCANDIR')      OR define('CNF_DEFAULT_SCANDIR', '.');
