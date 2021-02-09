3723ce0048347de70b34cc2b894b11f75a9cc8a1
Kang Cahya <cahyadyazin@yahoo.com>
2021-02-10 05:41:23 +0700

    remove frame

--- index.php
+++ index.php
@@ -80,6 +80,13 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  	<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    	<li class="nav-item">
+                            <a class="nav-link" href="<?=iframeUrl($_REQUEST['a']);?>" style="color: #FFFFFF;"><abbr title="Click here if you want to see on Real Page" class="initialism">Remove Frame</abbr></a>
+                        </li>
+                  	</ul>
+                </div>
               </div>
             </nav>
             <div>
